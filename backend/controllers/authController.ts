f5ec8ae9104c153c7d52965d64d911f93d0a379a
Denzel <denzelhooke@hotmail.com>
2024-02-01 23:33:27 -0800

    Added more form validity

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,19 +1,16 @@
 const asyncHandler = require("express-async-handler");
 
-import { Response, Request } from "express"
+import { Response, Request } from "express";
 
 const register = asyncHandler((res: any, req: Request) => {
+  console.log(JSON.parse(req.body));
+  // const { username, password } = req.body
 
-    console.log(JSON.parse(req.body))
-    // const { username, password } = req.body
-        
-    
-    // if(!username || !password) {
-    //     throw new Error("Hello")
-    // }
-})
+  // if(!username || !password) {
+  //     throw new Error("Hello")
+  // }
+});
 
 module.exports = {
-    register
-}
-
+  register,
+};
--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, ChangeEventHandler, ChangeEvent } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
-import axios from "axios"
+import axios from "axios";
 
 type FormFields = {
   username: string;
@@ -12,47 +12,89 @@ type FormFields = {
 const RegisterForm = () => {
   const mutation = useMutation({
     mutationFn: (data: FormFields) => {
-      return axios.post('http://localhost:3000/api/auth/register', data)
-    }
-  })
+      return axios.post("http://localhost:3000/api/auth/register", data);
+    },
+  });
 
   const {
     register,
     handleSubmit,
-    formState: { errors }
-  } = useForm<FormFields>();
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    defaultValues: {
+      username: "JohnDoe",
+    },
+  });
 
-  const onSubmit: SubmitHandler<FormFields> = (data) => {
-    //Data gets converted to object containing our fields rather than a formElement Event object. 
-    console.log(data)
-  }
-  
-  // HandleSubmit checks if our fields are all valid and prevents default behaviour of form submit. 
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      //Data gets converted to object containing our fields rather than a formElement Event object.
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      console.log(data);
+      throw new Error();
+    } catch {
+      setError("root", {
+        message: "This username is already taken",
+      });
+    }
+  };
 
+  // HandleSubmit checks if our fields are all valid and prevents default behaviour of form submit.
+
+  const passwordCheck = (string: string) => {
+    if (!string.includes("@")) {
+      return 'Password must contain "@"';
+    }
+
+    return true;
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input
-        {...register("username", { required: "Username required" , })}
+        {...register("username", { required: "Username required" })}
         placeholder="Username"
         type="text"
         name="username"
       />
-      { errors.username && <div>{errors.username.message}</div> }
+      {errors.username && <div>{errors.username.message}</div>}
 
-      <input {...register("password1", { required: "Password required", minLength: 8, validate: (string) => string.includes('@') })} placeholder="Password" type="text"
-      name="password1"/>
-      { errors.password1 && <div>{errors.password1.message}</div> }
+      {/* Validate: Validate allows a function to enable custom input validators. If they validate to true, the form passes, else they validate to false */}
+      <input
+        {...register("password1", {
+          required: "Password required",
+          validate: (string) => passwordCheck(string),
+          minLength: {
+            value: 8,
+            message: " Password must have at least 8 characters",
+          },
+        })}
+        placeholder="Password"
+        type="text"
+        name="password1"
+      />
+      {errors.password1 && <div>{errors.password1.message}</div>}
 
       <input
-        {...register("password2", { required: "Please confirm password", minLength: 8 })}
+        {...register("password2", {
+          required: "Please confirm password",
+          validate: (string) => passwordCheck(string),
+          minLength: {
+            value: 8,
+            message: " Password must have at least 8 characters",
+          },
+        })}
         placeholder="Confirm Password"
         type="text"
         name="password2"
-        />
-        { errors.password2 && <div>{errors.password2.message}</div> }
+      />
+      {errors.password2 && <div>{errors.password2.message}</div>}
 
-      <input type="submit" />
+      <button disabled={isSubmitting} type="submit">
+        {isSubmitting ? "LOADING" : "Submit"}
+      </button>
+      {errors.root && <div>{errors.root.message}</div>}
     </form>
   );
 };
